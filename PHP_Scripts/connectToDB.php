8b9f2c700560601487261c34a04ebf2c82fed2ab
Jeremy C <jeremy.comeo@gmail.com>
2017-03-24 11:24:47 -0400

    Calm before the storm
    Remove last name
    comitting, syncing, and cloning to new pc's apache/htdocs

--- PHP_Scripts/connectToDB.php
+++ PHP_Scripts/connectToDB.php
@@ -1,9 +1,18 @@
 <?php
+	
+	/*	
+		Domain to connect to with credentials - this is all for testing
+		In the future there will be a limited-access account that will access the db
+		to create users, delete users, and grant users permissions
+	*/
 	$dsn = 'mysql:dbname=test;host=127.0.0.1';
 	$user = 'root';
 	$pass = '';
 	
-	
+	/*
+		PHP Data object that we provide args in order to connect to the database
+		Will be used to execute commands to get and change records
+	*/
 	$db = new PDO($dsn, $user, $password);
 	
 	//Use a try catch in the future to get connection success /errors
